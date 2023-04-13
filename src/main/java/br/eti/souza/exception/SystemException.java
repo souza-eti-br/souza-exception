5b7ad1f64b827d49a4f1cbd056638c285cc0ac6b
Alan Moraes Souza <alan@souza.eti.br>
2023-04-12 21:40:41 -0300

    Initial version

---
+++ src/main/java/br/eti/souza/exception/SystemException.java
@@ -0,0 +1,52 @@
+package br.eti.souza.exception;
+
+/**
+ * Exceções causada pelo sistema.
+ * @author Alan Moraes Souza
+ */
+public class SystemException extends Exception {
+
+    /** Mensagem internacionalizada da exceção. */
+    private final I18nMessage i18nMessage;
+
+    /** Construtor padrão. */
+    public SystemException() {
+        this.i18nMessage = null;
+    }
+
+    /**
+     * Construtor que define a mensagem internacionalizada da exceção.
+     * @param message Mensagem internacionalizada da exceção. (a chave é utilizada como a mensagem da exceção)
+     */
+    public SystemException(I18nMessage message) {
+        super(message != null ? message.getKey() : (String) null);
+        this.i18nMessage = message;
+    }
+
+    /**
+     * Construtor que define a causa da exceção.
+     * @param cause Causa da exceção.
+     */
+    public SystemException(Throwable cause) {
+        super(cause);
+        this.i18nMessage = null;
+    }
+
+    /**
+     * Construtor que define a mensagem internacionalizada e a causa da exceção.
+     * @param message Mensagem internacionalizada da exceção. (a chave é utilizada como a mensagem da exceção)
+     * @param cause Causa da exceção.
+     */
+    public SystemException(I18nMessage message, Throwable cause) {
+        super((message != null ? message.getKey() : (String) null), cause);
+        this.i18nMessage = message;
+    }
+
+    /**
+     * Retorna a mensagem internacionalizada da exceção.
+     * @return Mensagem internacionalizada da exceção.
+     */
+    public I18nMessage getI18nMessage() {
+        return this.i18nMessage;
+    }
+}
