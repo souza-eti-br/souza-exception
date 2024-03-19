211a5fa6df5af2297bc0eab8e68df9a3638e0e54
Alan Moraes Souza <alan@souza.eti.br>
2024-03-19 20:34:00 -0300

    Corrigindo formatação.

--- src/main/java/br/eti/souza/exception/I18nMessage.java
+++ src/main/java/br/eti/souza/exception/I18nMessage.java
@@ -9,44 +9,44 @@ import java.util.List;
  */
 public class I18nMessage {
 
-  /** Chave da mensagem. */
-  private final String key;
-  /** Argumentos da mensagem. */
-  private final List<String> args = new ArrayList<>();
+    /** Chave da mensagem. */
+    private final String key;
+    /** Argumentos da mensagem. */
+    private final List<String> args = new ArrayList<>();
 
-  /**
-   * Construtor que define a chave da mensagem sem argumentos.
-   * @param key Chave da mensagem. (se null, usa "null").
-   */
-  public I18nMessage(String key) {
-    this.key = (key == null ? "null" : key);
-  }
-
-  /**
-   * Construtor que define a chave e argumentos da mensagem.
-   * @param key Chave da mensagem. (se null, usa "null").
-   * @param args Argumentos da mensagem. (se algum argumento for null, usa "null").
-   */
-  public I18nMessage(String key, String... args) {
-    this(key);
-    for (var arg : args) {
-      this.args.add(arg == null ? "null" : arg);
+    /**
+     * Construtor que define a chave da mensagem sem argumentos.
+     * @param key Chave da mensagem. (se null, usa "null").
+     */
+    public I18nMessage(String key) {
+        this.key = (key == null ? "null" : key);
     }
-  }
 
-  /**
-   * Retorna a chave da mensagem.
-   * @return Chave da mensagem.
-   */
-  public String getKey() {
-    return this.key;
-  }
+    /**
+     * Construtor que define a chave e argumentos da mensagem.
+     * @param key Chave da mensagem. (se null, usa "null").
+     * @param args Argumentos da mensagem. (se algum argumento for null, usa "null").
+     */
+    public I18nMessage(String key, String... args) {
+        this(key);
+        for (var arg : args) {
+            this.args.add(arg == null ? "null" : arg);
+        }
+    }
 
-  /**
-   * Retorna os argumentos da mensagem.
-   * @return Argumentos da mensagem.
-   */
-  public String[] getArgs() {
-    return this.args.toArray(String[]::new);
-  }
+    /**
+     * Retorna a chave da mensagem.
+     * @return Chave da mensagem.
+     */
+    public String getKey() {
+        return this.key;
+    }
+
+    /**
+     * Retorna os argumentos da mensagem.
+     * @return Argumentos da mensagem.
+     */
+    public String[] getArgs() {
+        return this.args.toArray(String[]::new);
+    }
 }
